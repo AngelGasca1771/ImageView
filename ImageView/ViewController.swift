910d7586caf015deb992a11219c87f1adb2f13aa
ANGEL GASCA-CARDONA <rl230154@rlas-macair-31.rlas-116.org>
2018-12-11 11:52:07 -0600

    Initial Commit

---
+++ ImageView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by ANGEL GASCA-CARDONA on 9/6/18.
+//  Copyright © 2018 ANGEL GASCA-CARDONA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var myImage = #imageLiteral(resourceName: "Minecraft")
+    var myImage2 = #imageLiteral(resourceName: "Panther")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
